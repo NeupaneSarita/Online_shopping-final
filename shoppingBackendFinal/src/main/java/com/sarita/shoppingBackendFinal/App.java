8eef2a09d01f8dc6c0c42484c7da34d5331d7098
Sarita Neupane <neupanesarita111@gmail.com>
2018-12-14 16:42:10 +0545

    project setup

---
+++ shoppingBackendFinal/src/main/java/com/sarita/shoppingBackendFinal/App.java
@@ -0,0 +1,13 @@
+package com.sarita.shoppingBackendFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
